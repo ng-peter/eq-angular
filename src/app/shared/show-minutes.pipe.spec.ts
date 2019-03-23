9218b106db619f361f4661488532d26103576fc0
Peter Ng <ng.peter@gmail.com>
2019-03-23 13:23:56 -0400

    class08 demo

---
+++ src/app/shared/show-minutes.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ShowMinutesPipe } from './show-minutes.pipe';
+
+describe('ShowMinutesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ShowMinutesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
